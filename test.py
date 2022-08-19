a1e3ffe54dcabb782c3cddf8cefb51427b0b14b3
ShinJaehun <gunsmoke.shin@gmail.com>
2022-08-19 20:30:07 +0900

    rectangle

--- test.py
+++ test.py
@@ -14,8 +14,13 @@ test_font = pygame.font.Font('assets/font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('assets/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('assets/graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
-snail_surface = pygame.image.load('assets/graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_surf = pygame.image.load('assets/graphics/snail/snail1.png').convert_alpha()
+# snail_x_pos = 600
+# rect를 사용해서 animation 구현하기
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('assets/graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
 
 while True:
 	for event in pygame.event.get():
@@ -29,10 +34,19 @@ while True:
 	screen.blit(sky_surface, (0, 0))
 	screen.blit(ground_surface, (0, 300))
 	screen.blit(text_surface, (300, 50))
-	snail_x_pos -= 4
-	if (snail_x_pos < -100):
-		snail_x_pos = 800
-	screen.blit(snail_surface, (snail_x_pos, 250))
+	# snail_x_pos -= 4
+	# if (snail_x_pos < -100):
+	# 	snail_x_pos = 800
+	# screen.blit(snail_surface, (snail_x_pos, 250))
+	# rect의 x, y, right, left 등을 이용해서 조작 가능!
+	snail_rect.x -= 4
+	if (snail_rect.right <= 0):
+		snail_rect.left = 800
+	screen.blit(snail_surf, snail_rect)
+	
+	# print(player_rect.left)
+	# player_rect.left += 1
+	screen.blit(player_surf, player_rect)
 
 	pygame.display.update()
 	clock.tick(60)
