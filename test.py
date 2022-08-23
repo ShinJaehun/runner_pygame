acd5caf3394c3cf531bea039ee284390fb0026dc
ShinJaehun <gunsmoke.shin@gmail.com>
2022-08-23 21:41:58 +0900

    better enemy spawn logic

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from cgi import test
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
 	current_time = int(pygame.time.get_ticks() / 1000) - start_time
@@ -8,6 +9,28 @@ def display_score():
 	score_rect = score_surf.get_rect(center = (400, 50))
 	screen.blit(score_surf, score_rect)
 	return current_time
+
+def obstacle_movement(obstacle_list):
+	if obstacle_list:
+		for obstacle_rect in obstacle_list:
+			obstacle_rect.x -= 5
+			# screen.blit(snail_surf, obstacle_rect)
+			# 근데 이런 식으로 검사하기 보다 어떤 스위치를 만들어서 넘기는게 낫지 않나?
+			if obstacle_rect.bottom == 300:
+				screen.blit(snail_surf, obstacle_rect)
+			else:
+				screen.blit(fly_surf, obstacle_rect)
+		obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100] # 이걸로 화면을 벗어나는 오브젝트 삭제
+		# print(obstacle_list)
+		return obstacle_list
+	else: return []
+
+def collision(player, obstacles):
+	if obstacles:
+		for obstacle_rect in obstacles :
+			if player.colliderect(obstacle_rect):
+				return False
+	return True
 
 pygame.init()
 
@@ -23,8 +46,12 @@ score = 0
 sky_surface = pygame.image.load('assets/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('assets/graphics/ground.png').convert()
 
+# obstacles
 snail_surf = pygame.image.load('assets/graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+# snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+fly_surf = pygame.image.load('assets/graphics/Fly/Fly1.png').convert_alpha()
+
+obstacle_rect_list = []
 
 player_surf = pygame.image.load('assets/graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
@@ -32,7 +59,6 @@ player_gravity = 0
 
 # intro screen
 player_stand = pygame.image.load('assets/graphics/Player/player_stand.png').convert_alpha()
-# player_stand = pygame.transform.scale2x(player_stand)
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
 
@@ -42,11 +68,16 @@ game_name_rect = game_name.get_rect(center = (400, 80))
 game_message = test_font.render('Press space to run', False, (111, 196, 169))
 game_message_rect = game_message.get_rect(center = (400, 320))
 
+# timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
+
 		if game_active:
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if player_rect.collidepoint(event.pos): 
@@ -57,8 +88,15 @@ while True:
 		else:
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE :
 				game_active = True
-				snail_rect.left = 800
+				# snail_rect.left = 800
 				start_time = int(pygame.time.get_ticks() / 1000)
+
+		if event.type == obstacle_timer and game_active:
+			# obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900, 1100), 300)))
+			if randint(0, 2):
+				obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900, 1100), 300)))
+			else:
+				obstacle_rect_list.append(fly_surf.get_rect(bottomright = (randint(900, 1100), 210)))
 
 	if game_active:
 		screen.blit(sky_surface, (0, 0))
@@ -66,9 +104,9 @@ while True:
 
 		score = display_score()
 
-		snail_rect.x -= 4
-		if (snail_rect.right <= 0): snail_rect.left = 800
-		screen.blit(snail_surf, snail_rect)
+		# snail_rect.x -= 4
+		# if (snail_rect.right <= 0): snail_rect.left = 800
+		# screen.blit(snail_surf, snail_rect)
 
 		# player
 		player_gravity += 1
@@ -76,11 +114,19 @@ while True:
 		if player_rect.bottom >= 300: player_rect.bottom = 300
 		screen.blit(player_surf, player_rect)
 
-		if snail_rect.colliderect(player_rect):
-			game_active = False
+		# obstacle movement
+		obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+		# collision
+		# if snail_rect.colliderect(player_rect):
+		# 	game_active = False
+		game_active = collision(player_rect, obstacle_rect_list)
 	else:
 		screen.fill((94, 129, 162))
 		screen.blit(player_stand, player_stand_rect)
+		obstacle_rect_list.clear()
+		player_rect.midbottom = (80,300)
+		player_gravity = 0
 
 		score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
 		score_message_rect = score_message.get_rect(center = (400, 330))
